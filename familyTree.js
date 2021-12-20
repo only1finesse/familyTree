7e56fb1d625b3d7ef07e3c2b6efb4e58bae5100e
only1finesse <72461240+only1finesse@users.noreply.github.com>
2021-12-20 09:32:13 -0800

    Create familyTree.js
    Creating objects with object literal

---
+++ familyTree.js
@@ -0,0 +1,32 @@
+let generationOne = {
+    name = "Prince George",
+    parents = {
+        mother = 'Catherine',
+        father = 'Prince William',
+    },
+    childOf() {
+        console.log(this.parents)
+    }
+}
+
+let generationTwo = {
+    name = "Prince Harry",
+    parents = {
+        mother = 'Diana',
+        father = 'Charles',
+    },
+    childOf() {
+        console.log(this.parents)
+    }
+}
+
+let generationThree = {
+    name = "Prince Andrew",
+    parents = {
+        mother = 'Prince Phillip',
+        father = 'Queen Elizabeth II',
+    },
+    childOf() {
+        console.log(this.parents)
+    }
+}
